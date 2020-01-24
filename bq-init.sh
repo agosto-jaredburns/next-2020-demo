ec62574e7f4cf60af891d265bc67758cfea51ae3
agosto-jaredburns <45371148+agosto-jaredburns@users.noreply.github.com>
2020-01-24 13:02:42 -0600

    Load source data to BQ

---
+++ bq-init.sh
@@ -0,0 +1,23 @@
+bq --location=US load \
+--replace \
+--source_format=CSV \
+--skip_leading_rows=1 \
+next2020.customer \
+gs://next-2020-jb/Customer.csv \
+customer_Id:NUMERIC,DOB:STRING,Gender:STRING,city_code:INTEGER
+
+bq --location=US load \
+--replace \
+--source_format=CSV \
+--skip_leading_rows=1 \
+next2020.transactions \
+gs://next-2020-jb/Transactions.csv \
+transaction_id:NUMERIC,cust_id:NUMERIC,tran_date:STRING,prod_subcat_code:INTEGER,prod_cat_code:INTEGER,Qty:NUMERIC,Rate:NUMERIC,Tax:FLOAT,total_amt:FLOAT,Store_type:STRING
+
+bq --location=US load \
+--replace \
+--source_format=CSV \
+--skip_leading_rows=1 \
+next2020.prod_cat_info \
+gs://next-2020-jb/prod_cat_info.csv \
+prod_cat_code:INTEGER,prod_cat:STRING,prod_sub_cat_code:INTEGER,prod_subcat:STRING
